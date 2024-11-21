79ceff8ab2692a064d4c0595bf739d3e163c9a65
Caleb Nasio <nascal3@gmail.com>
2024-11-21 17:47:16 +0300

    Added routers to app

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import './App.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import {useReducer, useState} from "react";
+import HomePage from "./pages/HomePage";
+import Dashboard from "./pages/Dashboard";
 
 function App() {
     function reducer(state, action) {
@@ -25,19 +28,29 @@ function App() {
     const [state, dispatch] = useReducer(reducer, { balance: 0});
 
   return (
-      <div className="App">
-          <strong>Balance: </strong>{state.balance}
-          <div>
-              <input
-                  onChange={(e) => setAmount(e.target.value)} type='text'
-                  value={amount}
-              />
+      <>
+          <div className="App">
+              <strong>Balance: </strong>{state.balance}
+              <div>
+                  <input
+                      onChange={(e) => setAmount(e.target.value)} type='text'
+                      value={amount}
+                  />
+              </div>
+              <div>
+                  <button onClick={() => dispatch({type: 'deposit', payload: Number(amount)})}>Deposit</button>
+                  <button onClick={() => dispatch({type: 'withdraw', payload: Number(amount)})}>Withdraw</button>
+              </div>
           </div>
-          <div>
-              <button onClick={() => dispatch({type: 'deposit', payload: Number(amount)})} >Deposit</button>
-              <button onClick={()=> dispatch({type: 'withdraw', payload: Number(amount)})} >Withdraw</button>
-          </div>
-      </div>
+          <BrowserRouter>
+              <Routes>
+                  <Route path="/" element={<HomePage />} />
+                  <Route path="/dashboard" element={<Dashboard />} />
+                  <Route path="*" element={<h1>404</h1>} />
+              </Routes>
+          </BrowserRouter>
+      </>
+
   )
 
 }
